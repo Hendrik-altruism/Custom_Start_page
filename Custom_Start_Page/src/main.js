4b3606f2b4a997c373cd093d12d9ecd1602a7766
Hendrik Oude Hengel <hendrik.oude@smail.inf.h-brs.de>
2024-12-24 11:17:37 +0100

    new

--- Custom_Start_Page/src/main.js
+++ Custom_Start_Page/src/main.js
@@ -3,12 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { openDB } from './assets/indexedDB'
 
 const app = createApp(App)
 
 app.use(router)
 
 app.mount('#app')
-
-window.onload = await openDB();
